5dd4e503a183c9774678dfea2dcad5eebbdc06b6
Charles <hi@charlesify.com>
2026-01-03 15:59:41 +0800

    feat: add vite-plugin-compression to optimize build output and update related dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,20 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig(({ isSsrBuild }) => ({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+    viteCompression({
+      algorithm: "gzip", // 默认就是 gzip
+      ext: ".gz",
+      threshold: 10240, // 只有超过 10KB 的文件才压缩
+      deleteOriginFile: false, // 不要删除源文件，否则老浏览器会打不开
+    }),
+  ],
   build: {
     rollupOptions: {
       output: {
